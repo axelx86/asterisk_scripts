226809935bce9b7cb7fad347910e77d048aec593
Alexey <64968436+axelx86@users.noreply.github.com>
2021-10-18 16:47:01 +0300

    added alarm scripts
    if somebody call 8800

---
+++ alarm.sh/8800_Routing.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+WHO=$1; DID=$2; CID=$3;
+ChID="yourgroupid"
+#URL="https://api.telegram.org/bot$token"; token="452852860:AAHOIugk_Jr8f_9EDNm6jmo3ebQOT4J8v1Y"
+#mtd_send1=$URL/sendMessage
+TXT="#WARNING 🔥 $WHO 🔥 CallerID: $CID 🔥 Exten: $DID"
+#"Клиент: $WHO. Попытка международного вызова на номер $DID с номера $CID"
+JSONT='{ "chat_id" : "'$ChID'" , "text" : "'$TXT'" }'
+
+    send_text()
+    {
+    #sendMessage
+    /usr/bin/curl --header 'Content-Type: application/json' --request 'POST' --data "$JSONT" https://api.telegram.org/bot"youbotid"/sendMessage
+    }
+
+
+send_text
